3d40e8654c5759ee88f114c56da0597b3fb5926f
elenalazarus <35200816+elenalazarus@users.noreply.github.com>
2018-03-12 01:08:30 +0200

    Update battle.py

--- battle.py
+++ battle.py
@@ -22,10 +22,14 @@ def read_field(path):
 
 
 def has_ship(field, coordinate):
-    if field[coordinate] == '*':
-        return True
+    coordinate2 = str(coordinate[0] + str(coordinate[1]))
+    if type(field) == dict:
+        if field[coordinate2] == '*':
+            return True
     else:
-        return False
+        if coordinate in field:
+            return True
+    return False
 
 
 def ship_size(field, coordinate):
@@ -77,24 +81,42 @@ def ship_size(field, coordinate):
                         i += 1
                 except KeyError:
                     break
-        return length
+    return length
 
 
 def is_valid(field):
     only_ships = dict()
-    for key in field:
-        if field[key] == '*':
-            only_ships[key] = field[key]
     if len(field) != 100 or len(only_ships) != 20:
         return False
 
     return True
 
 
+def frame(field, ship):
+    border = []
+    new_field = []
+    for cell in ship:
+        border.append((chr(ord(cell[0]) - 1), cell[1] - 1))
+        border.append((chr(ord(cell[0])), cell[1] - 1))
+        border.append((chr(ord(cell[0]) + 1), cell[1] - 1))
+        border.append((chr(ord(cell[0]) + 1), cell[1]))
+        border.append((chr(ord(cell[0]) + 1), cell[1] + 1))
+        border.append((chr(ord(cell[0])), cell[1] + 1))
+        border.append((chr(ord(cell[0]) - 1), cell[1] + 1))
+        border.append((chr(ord(cell[0]) - 1), cell[1]))
+    for i in border:
+        if i in ship:
+            border.remove(i)
+    for cell in border:
+        for line in field:
+            if cell in line:
+                line.insert(line.index(cell), ' ')
+                line.remove(cell)
+    return field
+
 
 def generate_field():
     field = []
-    delete = []
     for numb in range(1, 11):
         line = []
         for letter in range(10):
@@ -108,36 +130,49 @@ def generate_field():
         what_line = random.choice(field)
         point = random.choice(what_line)
         what_way = random.choice(way)
+        new_field = copy.copy(field)
         while True:
             try:
                 if what_way == 'vertical':
                     while point == "*" or point == ' ' or point[1] + ship > 10:
                         what_line = random.choice(field)
                         point = random.choice(what_line)
+                    delete = []
                     while size != ship:
                         what_line.insert(what_line.index(point), '*')
                         delete.append(point)
+                        what_line.remove(point)
                         point = [point[0], point[1] + 1]
                         point = tuple(point)
                         what_line = field[point[1] - 1]
                         size += 1
+                    field = frame(field, delete)
                 else:
-                    while ord(point[0]) - 97 + ship > 10:
+                    while point == "*" or point == ' ' or ord(point[0]) - 97 + ship > 10:
                         what_line = random.choice(field)
                         point = random.choice(what_line)
+                    delete = []
                     while size != ship:
                         what_line.insert(what_line.index(point), '*')
                         delete.append(point)
+                        what_line.remove(point)
                         point = [chr(ord(point[0]) + 1), point[1]]
                         point = tuple(point)
                         size += 1
-            except IndexError:
-                continue
+                    field = frame(field, delete)
+            except:
+                generate_field()
             break
+    print()
+    norm_field = []
     for line in field:
-        print(line)
-
-
+        new_line = []
+        for elem in line:
+            if elem != '*':
+                new_line.append(' ')
+            else:
+                new_line.append('*')
+        print(new_line)
 
 
 def main():
@@ -145,8 +180,9 @@ def main():
     # print(field)
     # print(has_ship(field, ("b1")))
     # print(ship_size(field, "j10"))
-    print(is_valid(field))
-    print(generate_field())
+    # print(is_valid(field))
+    generate_field()
+
 
 
 main()
