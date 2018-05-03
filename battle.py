83ed0bed1de21d4d9460bbbf70b650a83ad0912f
elenalazarus <35200816+elenalazarus@users.noreply.github.com>
2018-05-04 00:02:49 +0300

    Update battle.py

--- battle.py
+++ battle.py
@@ -69,14 +69,16 @@ def ship_size(field, coordinate):
         i = 0
         while True:
             try:
-                while only_ships[str(chr(ord(coordinate[0]) - i) +
+                while only_ships[str(
+                                     chr(ord(coordinate[0]) - i) +
                                  coordinate[1])] == '*':
                     length += 1
                     i += 1
             except KeyError:
                 i = 1
                 try:
-                    while only_ships[str(chr(ord(coordinate[0]) + i) +
+                    while only_ships[str(
+                                         chr(ord(coordinate[0]) + i) +
                                      coordinate[1])] == '*':
                         length += 1
                         i += 1
@@ -107,11 +109,15 @@ def is_valid(field):
     :param field: dict
     :return: True or False
     '''
-    only_ships = dict()
-    if len(field) != 100 or len(only_ships) != 20:
+    ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
+    try:
+        for ship in field:
+            if len(ship) in ships:
+                ships.remove(len(ship))
+        if len(ships) == 0:
+            return True
+    except:
         return False
-
-    return True
 
 
 def frame(field, ship):
@@ -122,108 +128,107 @@ def frame(field, ship):
     :return: list
     '''
     border = []
-    new_field = []
     for cell in ship:
-        border.append((chr(ord(cell[0]) - 1), cell[1] - 1))
-        border.append((chr(ord(cell[0])), cell[1] - 1))
-        border.append((chr(ord(cell[0]) + 1), cell[1] - 1))
-        border.append((chr(ord(cell[0]) + 1), cell[1]))
-        border.append((chr(ord(cell[0]) + 1), cell[1] + 1))
-        border.append((chr(ord(cell[0])), cell[1] + 1))
-        border.append((chr(ord(cell[0]) - 1), cell[1] + 1))
-        border.append((chr(ord(cell[0]) - 1), cell[1]))
-    for i in border:
+        for i in {-1, 0, 1}:
+            for k in {-1, 0, 1}:
+                border.append((chr(ord(cell[0]) + i), cell[1] + k))
+    for i in set(border):
         if i in ship:
             border.remove(i)
     for cell in border:
         for line in field:
             if cell in line:
-                line.insert(line.index(cell), ' ')
                 line.remove(cell)
     return field
 
 
 def generate_field():
     '''
-    Generates a field
-    :return: list, list
+    Field generation
     '''
     shipses = []
+    all_ships = []
     field = []
+    all_field = []
     for numb in range(1, 11):
         line = []
         for letter in range(10):
             coor = tuple([chr(letter + 97), numb])
             line.append(coor)
+            all_field.append(coor)
         field.append(line)
+    # Future ships
     ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
     for ship in ships:
         size = 0
         way = ['horizontal', 'vertical']
-        what_line = random.choice(field)
-        point = random.choice(what_line)
-        what_way = random.choice(way)
         while True:
+            pre_field = copy.copy(field)
             try:
+                delete = []
+                # Choose the line where we will start a ship
+                what_line = random.choice(field)
+                if len(what_line) == 0:
+                    # If there is no possibility to put a ship
+                    while len(what_line) == 0:
+                        what_line = random.choice(field)
+                point = random.choice(what_line)
+                what_way = random.choice(way)
                 if what_way == 'vertical':
-                    while point == "*" or point == ' ' or point[1] + ship > 10:
+                    # Check if we cen put a large ship there
+                    while point[1] + ship > 10:
                         what_line = random.choice(field)
                         point = random.choice(what_line)
-                    delete = []
-                    shipses.append([point, (1, ship), False, []])
+                    shipses.append([point, ship, False, []])
                     while size != ship:
-                        what_line.insert(what_line.index(point), '*')
                         delete.append(point)
                         what_line.remove(point)
-                        point = [point[0], point[1] + 1]
-                        point = tuple(point)
+                        point = (point[0], point[1] + 1)
                         what_line = field[point[1] - 1]
                         size += 1
-
-                    field = frame(field, delete)
                 else:
-                    while point == "*" or point == ' ' or ord(
-                            point[0]) - 97 + ship > 10:
+                    while ord(point[0]) - 97 + ship > 10:
                         what_line = random.choice(field)
                         point = random.choice(what_line)
-                    delete = []
-                    shipses.append([point, (ship, 1), True, []])
+                    shipses.append([point, ship, True, []])
                     while size != ship:
-                        what_line.insert(what_line.index(point), '*')
                         delete.append(point)
                         what_line.remove(point)
-                        point = [chr(ord(point[0]) + 1), point[1]]
-                        point = tuple(point)
+                        point = (chr(ord(point[0]) + 1), point[1])
                         size += 1
-
-                    field = frame(field, delete)
             except:
-                generate_field()
+                field = copy.copy(pre_field)
+                continue
             break
-    norm_field = []
-    for line in field:
-        new_line = []
-        for elem in line:
-            if elem != '*':
-                new_line.append(' ')
+        all_ships.append(delete)
+        field = frame(field, delete)
+    return all_ships, shipses
+
+
+def field_to_str(all_ships):
+    '''
+    Output a field for user
+    '''
+    field = ''
+    str_field = []
+    for ship in all_ships:
+        for coor in ship:
+            str_field.append(coor)
+    sorted(str_field)
+    for i in range(10):
+        line = ""
+        for k in range(1, 11):
+            point = (chr(i + 97), k)
+            if point in str_field:
+                line += '■'
             else:
-                new_line.append('*')
-        norm_field.append(new_line)
-    return norm_field, shipses
-
-
-def main():
-    '''
-    Boss of all functions
-    :return: None
-    '''
-    field = read_field('field.txt')
-    # print(field)
-    # print(has_ship(field, ("b1")))
-    # print(ship_size(field, "j10"))
-    # print(is_valid(field))
-    for line in generate_field()[0]:
+                line += "□"
         print(line)
+        field += '\n' + line
 
 
-main()
+if __name__ == "__main__":
+    all_ships = generate_field()[0]
+    while not is_valid(all_ships):
+        all_ships = generate_field()[0]
+    print(field_to_str(all_ships))
