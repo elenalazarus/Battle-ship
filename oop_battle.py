7873e0b1531fc03e895aee16561349dc48686ae8
elenalazarus <35200816+elenalazarus@users.noreply.github.com>
2018-05-04 00:03:28 +0300

    Update oop_battle.py

--- oop_battle.py
+++ oop_battle.py
@@ -1,231 +1,7 @@
-import copy
-import random
-
-
-def read_field(path):
-    '''
-    (path) -> dict
-    Read field from file
-    '''
-    field = dict()
-    with open(path, 'r', encoding='utf-8') as f:
-        ship = []
-        numb = 1
-        for line in f:
-            letter = 0
-            while len(line) < 10:
-                line += ' '
-            for elem in line:
-                coor = chr(letter + 97) + str(numb)
-                if elem == '\n':
-                    elem = ' '
-                field[coor] = elem
-                letter += 1
-            numb += 1
-
-    return field
-
-
-def has_ship(field, coordinate):
-    '''
-    (dict, str) ->
-    Check if there is a ship in this cell
-    '''
-    coordinate2 = str(coordinate[0] + str(coordinate[1]))
-    if type(field) == dict:
-        if field[coordinate2] == '*':
-            return True
-    else:
-        if coordinate in field:
-            return True
-    return False
-
-
-def ship_size(field, coordinate):
-    '''
-    Check if coordinate which was put is a ship and return the length of a ship
-    :param field: dict
-    :param coordinate: str
-    :return: int
-    '''
-    only_ships = dict()
-    length = 0
-    if has_ship(field, coordinate):
-        for key in field:
-            if field[key] == '*':
-                only_ships[key] = field[key]
-
-        if len(coordinate) > 2:
-            coordinate = list(coordinate)
-            coordinate2 = []
-            coordinate2.extend(
-                [coordinate[0], str(coordinate[1] + coordinate[2])])
-            coordinate = copy.copy(coordinate2)
-        else:
-            coordinate = list(coordinate)
-        i = 0
-        while True:
-            try:
-                while only_ships[str(chr(ord(coordinate[0]) - i) +
-                                 coordinate[1])] == '*':
-                    length += 1
-                    i += 1
-            except KeyError:
-                i = 1
-                try:
-                    while only_ships[str(chr(ord(coordinate[0]) + i) +
-                                     coordinate[1])] == '*':
-                        length += 1
-                        i += 1
-                except KeyError:
-                    break
-        i = 1
-        while True:
-            try:
-                while only_ships[str(chr(ord(coordinate[0])) + str(
-                                int(coordinate[1]) - i))] == '*':
-                    length += 1
-                    i += 1
-            except KeyError:
-                i = 1
-                try:
-                    while only_ships[str(chr(ord(coordinate[0])) + str(
-                                    int(coordinate[1]) + i))] == '*':
-                        length += 1
-                        i += 1
-                except KeyError:
-                    break
-    return length
-
-
-def is_valid(field):
-    '''
-    Check if a field is valid for battleship
-    :param field: dict
-    :return: True or False
-    '''
-    only_ships = dict()
-    if len(field) != 100 or len(only_ships) != 20:
-        return False
-
-    return True
-
-
-def frame(field, ship):
-    '''
-    Return all coordinates around the ship in the field
-    :param field: list
-    :param ship: list
-    :return: list
-    '''
-    border = []
-    new_field = []
-    for cell in ship:
-        border.append((chr(ord(cell[0]) - 1), cell[1] - 1))
-        border.append((chr(ord(cell[0])), cell[1] - 1))
-        border.append((chr(ord(cell[0]) + 1), cell[1] - 1))
-        border.append((chr(ord(cell[0]) + 1), cell[1]))
-        border.append((chr(ord(cell[0]) + 1), cell[1] + 1))
-        border.append((chr(ord(cell[0])), cell[1] + 1))
-        border.append((chr(ord(cell[0]) - 1), cell[1] + 1))
-        border.append((chr(ord(cell[0]) - 1), cell[1]))
-    for i in border:
-        if i in ship:
-            border.remove(i)
-    for cell in border:
-        for line in field:
-            if cell in line:
-                line.insert(line.index(cell), ' ')
-                line.remove(cell)
-    return field
-
-
-class Field:
-    '''
-    Class where field creates
-    '''
-    def __init__(self):
-        def generate_field():
-            '''
-            Generates field
-            :return: list, list
-            '''
-            shipses = []
-            field = []
-            for numb in range(1, 11):
-                line = []
-                for letter in range(10):
-                    coor = tuple([chr(letter + 97), numb])
-                    line.append(coor)
-                field.append(line)
-            ships = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]
-            for ship in ships:
-                size = 0
-                way = ['horizontal', 'vertical']
-                what_line = random.choice(field)
-                point = random.choice(what_line)
-                what_way = random.choice(way)
-                while True:
-                    try:
-                        if what_way == 'vertical':
-                            while point == "*" \
-                                    or point == ' ' or point[1] + ship > 10:
-                                what_line = random.choice(field)
-                                point = random.choice(what_line)
-                            delete = []
-                            shipses.append([point, (1, ship), False, []])
-                            while size != ship:
-                                what_line.insert(what_line.index(point), '*')
-                                delete.append(point)
-                                what_line.remove(point)
-                                point = [point[0], point[1] + 1]
-                                point = tuple(point)
-                                what_line = field[point[1] - 1]
-                                size += 1
-
-                            field = frame(field, delete)
-                        else:
-                            while point == "*" or point == ' ' or ord(
-                                    point[0]) - 97 + ship > 10:
-                                what_line = random.choice(field)
-                                point = random.choice(what_line)
-                            delete = []
-                            shipses.append([point, (ship, 1), True, []])
-                            while size != ship:
-                                what_line.insert(what_line.index(point), '*')
-                                delete.append(point)
-                                what_line.remove(point)
-                                point = [chr(ord(point[0]) + 1), point[1]]
-                                point = tuple(point)
-                                size += 1
-
-                            field = frame(field, delete)
-                    except:
-                        generate_field()
-                    break
-            norm_field = []
-            for line in field:
-                new_line = []
-                for elem in line:
-                    if elem != '*':
-                        new_line.append(' ')
-                    else:
-                        new_line.append('*')
-                norm_field.append(new_line)
-            return norm_field, shipses
-
-        norm_field, ships = generate_field()
-        self.ships = []
-        self.norm_field = norm_field
-        for ship in ships:
-            shp = Ship(ship[0], ship[2], ship[1], ship[3])
-            self.ships.append(shp)
+from battle import generate_field
 
 
 class Ship:
-    '''
-    Class for initializing a ship
-    '''
     def __init__(self, bow, horizontal, length, hit):
         '''
         Initialize a ship
@@ -237,22 +13,118 @@ class Ship:
         self.bow = bow
         self.horizontal = horizontal
         self.__length = length
+        self.length = length
         self.__hit = hit[:]
+        self.hit = hit[:]
 
 
-class Game:
+class Field:
     '''
-    Let's play!
+    Initialize a field
     '''
+
     def __init__(self):
+        self.data = generate_field()
+        self.field = self.data[1]
+        self.ships = []
+        self.shipses = []
+        # Collectiong all data
+        for ship in self.data[0]:
+            for elem in ship:
+                self.shipses.append(elem)
+        self.shots = []
+        for ship in self.field:
+            ship2 = Ship(ship[0], ship[2], ship[1], ship[3])
+            self.ships.append(ship2)
+        self.all_field = []
+        for numb in range(1, 11):
+            for letter in range(10):
+                coor = tuple([chr(letter + 97), numb])
+                self.all_field.append(coor)
+
+    def shoot_at(self, tupl):
         '''
-        Initialize two players
+        Do a shoot in ship
+        :param tupl: tuple
+        :return: bool
         '''
-        self.__fields = [Field(), Field()]
-        name1 = input("Enter first player name: ")
-        name2 = input("Enter second player name: ")
-        self.__players = [Player(name1), Player(name2)]
-        self.__current_player = 1
+
+        if tupl not in self.all_field:
+            return False
+        for ship in self.ships:
+            for i in range(ship.length):
+                # Checking for every point in every ship
+                if ship.horizontal:
+                    point = (chr(ord(ship.bow[0]) + i), ship.bow[1])
+                else:
+                    point = (chr(ord(ship.bow[0])), ship.bow[1] + i)
+                # Return results
+                if point == tupl:
+                    ship.hit.append(tupl)
+                    self.all_field.remove(tupl)
+                    print("Yes! There is a ship!")
+                    if len(ship.hit) == ship.length:
+                        print("Wow! You killed it!")
+                    return True
+        print("No! You missed!")
+        self.all_field.remove(tupl)
+        self.shots.append(tupl)
+
+    def field_without_ships(self):
+        '''
+        Output field that player will use to orient in enemy's field
+        '''
+        field = ''
+        n = 0
+        for i in range(1, 11):
+            line = ""
+            for k in range(10):
+                point = (chr(k + 97), i)
+                for ship in self.ships:
+                    n = 0
+                    if point in ship.hit:
+                        line += "■"
+                        n += 1
+                        break
+                if point not in self.all_field and n == 0:
+                    line += '◯'
+                elif point in self.all_field:
+                    line += '□'
+
+            print(line)
+            field += '\n' + line
+
+    def field_with_ships(self):
+        '''
+        That's for player to see how enemy acts
+        '''
+        field = ''
+        for i in range(1, 11):
+            line = ""
+            for k in range(10):
+                point = (chr(k + 97), i)
+                # Outputing ships and empty spaces with symbols
+                if point in self.all_field and point not in self.shipses:
+                    line += '□'
+                elif point not in self.all_field and point in self.shipses:
+                    line += 'x'
+                elif point in self.shipses:
+                    line += "■"
+                elif point not in self.all_field:
+                    line += '◯'
+            print(line)
+            field += '\n' + line
+
+    def win_or_not(self):
+        '''
+        Consider if there is a winner
+        '''
+        i = 0
+        for ship in self.ships:
+            if len(ship.hit) == ship.length:
+                i += 1
+        if i == 10:
+            return True
 
 
 class Player:
@@ -267,7 +139,75 @@ class Player:
         Player.id += 1
 
     def read_position(self):
-        pass
+        '''
+        Initialize the act of a player
+        '''
+        try:
+            while True:
+                coor = input("Input coordinate as in example: a6" + '\n')
+                point = (coor[0], int(coor[1:]))
+                return point
+        except:
+            print("Input right data please")
+
+
+class Game:
+    '''
+    Let's play!
+    '''
+
+    def __init__(self):
+        '''
+        Initialize two players
+        '''
+        field1 = Field()
+        field2 = Field()
+        self.__fields = [field1, field2]
+        name1 = input("Enter first player name: ")
+        name2 = input("Enter second player name: ")
+        player1 = Player(name1)
+        player2 = Player(name2)
+        self.__players = [player1, player2]
+        self.__current_player = 1
+        while True:
+            # Let's start game!
+            if self.__current_player == 1:
+                print("Field of {}".format(name1))
+                print()
+                # Field of player
+                field1.field_with_ships()
+                print()
+                # Field of enemy
+                print("Field of {}".format(name2))
+                print()
+                field2.field_without_ships()
+                coor = player1.read_position()
+                a = field2.shoot_at(coor)
+                # If player make a good shot then he continues
+                if a:
+                    # If there is no ships he is the winner!
+                    if field2.win_or_not():
+                        print("YES! {} is winner".format(name1))
+                    continue
+                else:
+                    self.__current_player = 2
+
+            else:
+                print("Field of {}".format(name2))
+                print()
+                field2.field_with_ships()
+                print()
+                print("Field of {}".format(name1))
+                print()
+                field1.field_without_ships()
+                coor = player2.read_position()
+                a = field1.shoot_at(coor)
+                if a:
+                    if field1.win_or_not():
+                        print("YES! {} is winner".format(name2))
+                    continue
+                else:
+                    self.__current_player = 1
 
 
 def main():
@@ -276,5 +216,6 @@ def main():
     :return: None
     '''
     game = Game()
+
 
 main()
